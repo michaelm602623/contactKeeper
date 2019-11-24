3b63ed4e578dc4d4a162ab007e931e0dd996e65c
michaelm602 <48140179+michaelm602@users.noreply.github.com>
2019-11-24 12:50:47 -0800

    first try

---
+++ .history/client/src/components/contacts/ContactItem_20191121115312.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ContactItem = ({ contact }) => {
+  return (
+    <div className='card bg-light'>
+      <h3 className='text-primary text-left'>
+        {name}
+        {""}{" "}
+        <span
+          className={
+            "badge " +
+            (type === "professional" ? "badge-success" : "badge-primary")
+          }
+        >
+          {type}
+        </span>
+      </h3>
+    </div>
+  );
+};
+
+export default ContactItem;
---
+++ .history/routes/users_20191120195729.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResults } = require("express-validator/check");
+
+const User = require("../models/User");
+
+// @route   POST api/users
+// @desc    Register a user
+// @access  Public
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    res.send(req.body);
+  }
+);
+
+module.exports = router;
